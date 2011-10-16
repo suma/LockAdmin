7980c75051a9e0da693085264e1d49fb9f5bf942
suma <suma@users.sourceforge.jp>
2011-10-16 14:20:02 +0900

    reformat java code

--- src/com/suma/lockAdmin/SettingActivity.java
+++ src/com/suma/lockAdmin/SettingActivity.java
@@ -17,46 +17,46 @@ public class SettingActivity extends PreferenceActivity {
 
 	private static final String TAG = "LockAdmin";
 
-    private DevicePolicyManager mDevicePolicyManager;
-    private ComponentName mDeviceAdmin;
+	private DevicePolicyManager mDevicePolicyManager;
+	private ComponentName mDeviceAdmin;
 	private boolean mAdminActivated = false;
 
 	private CheckBoxPreference mDelayEnableCheckbox;
 	private ListPreference mDelayList;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        // Prepare to work with the DevicePolicyManager
-        mDevicePolicyManager = (DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE);
-        mDeviceAdmin = new ComponentName(this, AdminReceiver.class);
+		// Prepare to work with the DevicePolicyManager
+		mDevicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+		mDeviceAdmin = new ComponentName(this, AdminReceiver.class);
 
-        // Activate Device Administrator
-        Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-        intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mDeviceAdmin);
-        startActivityForResult(intent, RESULT_ENABLE);
+		// Activate Device Administrator
+		Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+		intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mDeviceAdmin);
+		startActivityForResult(intent, RESULT_ENABLE);
 
 		// Initialize activity
 		addPreferencesFromResource(R.xml.preference);
 
 		// Enable checkbox
-		mDelayEnableCheckbox = (CheckBoxPreference)findPreference("delay_enable");
+		mDelayEnableCheckbox = (CheckBoxPreference) findPreference("delay_enable");
 		mDelayEnableCheckbox.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
 			@Override
 			public boolean onPreferenceChange(Preference preference, Object o) {
-				return setDelayEnabled(((Boolean)o).booleanValue());
+				return setDelayEnabled(((Boolean) o).booleanValue());
 			}
 		});
 
 		// Delay listbox
-		mDelayList = (ListPreference)findPreference("delay_time");
+		mDelayList = (ListPreference) findPreference("delay_time");
 		mDelayList.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
 			@Override
 			public boolean onPreferenceChange(Preference preference, Object o) {
 				setTimeoutDelayFromList();
 
-				String entry = (String) mDelayList.getEntries()[mDelayList.findIndexOfValue((String)o)];
+				String entry = (String) mDelayList.getEntries()[mDelayList.findIndexOfValue((String) o)];
 				String summary = getString(R.string.pref_delay_time_summary, entry);
 				mDelayList.setSummary(summary);
 				return true;
@@ -68,16 +68,14 @@ public class SettingActivity extends PreferenceActivity {
 			mDelayList.setValueIndex(0);
 		}
 		updateDelaySummary();
-    }
+	}
 
-	private void updateDelaySummary()
-	{
-		String summary = getString(R.string.pref_delay_time_summary, (String)mDelayList.getEntry());
+	private void updateDelaySummary() {
+		String summary = getString(R.string.pref_delay_time_summary, (String) mDelayList.getEntry());
 		mDelayList.setSummary(summary);
 	}
 
-	boolean setDelayEnabled(boolean enable)
-	{
+	boolean setDelayEnabled(boolean enable) {
 		if (enable) {
 			return setTimeoutDelayFromList();
 		} else if (mAdminActivated) {
@@ -89,8 +87,7 @@ public class SettingActivity extends PreferenceActivity {
 		return false;
 	}
 
-	private boolean setTimeoutDelayFromList()
-	{
+	private boolean setTimeoutDelayFromList() {
 		if (mAdminActivated) {
 			Log.d(TAG, "Lock delay enabled");
 			int value = Integer.valueOf(mDelayList.getValue());
@@ -101,8 +98,8 @@ public class SettingActivity extends PreferenceActivity {
 		}
 	}
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == RESULT_ENABLE) {
 			if (resultCode == Activity.RESULT_OK && mDevicePolicyManager.isAdminActive(mDeviceAdmin)) {
 				mAdminActivated = true;
@@ -114,7 +111,7 @@ public class SettingActivity extends PreferenceActivity {
 			return;
 		}
 
-        super.onActivityResult(requestCode, resultCode, data);
-    }
+		super.onActivityResult(requestCode, resultCode, data);
+	}
 
 }
